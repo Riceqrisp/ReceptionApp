9ec8a393f076c63ed491f42a640525011d17ebc7
Tomas <tmoruszewicz@gmail.com>
2019-02-26 13:31:58 +0100

    initial commit

---
+++ ReceptionApp/ReceptionApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReceptionApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
